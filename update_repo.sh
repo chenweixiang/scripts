afa559d469069deae7871d77571d4c2239591032
Chen Weixiang <weixiangx.chen@outlook.com>
2017-06-14 08:53:31 +0800

    Remove no needed repo and add README.Alex

--- update_repo.sh
+++ update_repo.sh
@@ -26,17 +26,17 @@ echo -e "\n--- git status ---"
 git status
 
 
-echo
-echo -e "##################################"
-echo -e "### Update Repository: ~/gitdm ###"
-echo -e "##################################"
-echo -e "\n--- change directory ---"
-echo cd ~/gitdm
-cd ~/gitdm
-echo -e "\n--- git fetch --all ---"
-git fetch --all
-echo -e "\n--- git status ---"
-git status
+#echo
+#echo -e "##################################"
+#echo -e "### Update Repository: ~/gitdm ###"
+#echo -e "##################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/gitdm
+#cd ~/gitdm
+#echo -e "\n--- git fetch --all ---"
+#git fetch --all
+#echo -e "\n--- git status ---"
+#git status
 
 
 echo
@@ -52,43 +52,43 @@ echo -e "\n--- git status ---"
 git status
 
 
-echo
-echo -e "###################################"
-echo -e "### Update Repository: ~/sparse ###"
-echo -e "###################################"
-echo -e "\n--- change directory ---"
-echo cd ~/sparse
-cd ~/sparse
-echo -e "\n--- git fetch --all ---"
-git fetch --all
-echo -e "\n--- git status ---"
-git status
+#echo
+#echo -e "###################################"
+#echo -e "### Update Repository: ~/sparse ###"
+#echo -e "###################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/sparse
+#cd ~/sparse
+#echo -e "\n--- git fetch --all ---"
+#git fetch --all
+#echo -e "\n--- git status ---"
+#git status
 
 
-echo
-echo -e "##################################"
-echo -e "### Update Repository: ~/quilt ###"
-echo -e "##################################"
-echo -e "\n--- change directory ---"
-echo cd ~/quilt
-cd ~/quilt
-echo -e "\n--- git fetch --all ---"
-git fetch --all
-echo -e "\n--- git status ---"
-git status
+#echo
+#echo -e "##################################"
+#echo -e "### Update Repository: ~/quilt ###"
+#echo -e "##################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/quilt
+#cd ~/quilt
+#echo -e "\n--- git fetch --all ---"
+#git fetch --all
+#echo -e "\n--- git status ---"
+#git status
 
 
-echo
-echo -e "###################################"
-echo -e "### Update Repository: ~/smatch ###"
-echo -e "###################################"
-echo -e "\n--- change directory ---"
-echo cd ~/smatch
-cd ~/smatch
-echo -e "\n--- git fetch --all ---"
-git fetch --all
-echo -e "\n--- git status ---"
-git status
+#echo
+#echo -e "###################################"
+#echo -e "### Update Repository: ~/smatch ###"
+#echo -e "###################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/smatch
+#cd ~/smatch
+#echo -e "\n--- git fetch --all ---"
+#git fetch --all
+#echo -e "\n--- git status ---"
+#git status
 
 
 echo
@@ -104,15 +104,15 @@ echo -e "\n--- git status ---"
 git status
 
 
-echo
-echo -e "#################################"
-echo -e "### Update Repository: ~/mutt ###"
-echo -e "#################################"
-echo -e "\n--- change directory ---"
-echo cd ~/mutt
-cd ~/mutt
-echo -e "\n--- hg pull -u ---"
-hg pull -u
+#echo
+#echo -e "#################################"
+#echo -e "### Update Repository: ~/mutt ###"
+#echo -e "#################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/mutt
+#cd ~/mutt
+#echo -e "\n--- hg pull -u ---"
+#hg pull -u
 
 
 #echo
@@ -124,43 +124,43 @@ hg pull -u
 #git fetch --all
 
 
-echo
-echo -e "####################################"
-echo -e "### Update Repository: ~/systemd ###"
-echo -e "####################################"
-echo -e "\n--- change directory ---"
-echo cd ~/systemd
-cd ~/systemd
-echo -e "\n--- git fetch --all ---"
-git fetch --all
-echo -e "\n--- git status ---"
-git status
+#echo
+#echo -e "####################################"
+#echo -e "### Update Repository: ~/systemd ###"
+#echo -e "####################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/systemd
+#cd ~/systemd
+#echo -e "\n--- git fetch --all ---"
+#git fetch --all
+#echo -e "\n--- git status ---"
+#git status
 
 
-echo
-echo -e "####################################"
-echo -e "### Update Repository: ~/busybox ###"
-echo -e "####################################"
-echo -e "\n--- change directory ---"
-echo cd ~/busybox
-cd ~/busybox
-echo -e "\n--- git fetch --all ---"
-git fetch --all
-echo -e "\n--- git status ---"
-git status
+#echo
+#echo -e "####################################"
+#echo -e "### Update Repository: ~/busybox ###"
+#echo -e "####################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/busybox
+#cd ~/busybox
+#echo -e "\n--- git fetch --all ---"
+#git fetch --all
+#echo -e "\n--- git status ---"
+#git status
 
 
-echo
-echo -e "####################################"
-echo -e "### Update Repository: ~/u-boot ###"
-echo -e "####################################"
-echo -e "\n--- change directory ---"
-echo cd ~/u-boot
-cd ~/u-boot
-echo -e "\n--- git fetch --all ---"
-git fetch --all
-echo -e "\n--- git status ---"
-git status
+#echo
+#echo -e "####################################"
+#echo -e "### Update Repository: ~/u-boot ###"
+#echo -e "####################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/u-boot
+#cd ~/u-boot
+#echo -e "\n--- git fetch --all ---"
+#git fetch --all
+#echo -e "\n--- git status ---"
+#git status
 
 
 echo
@@ -176,17 +176,17 @@ echo -e "\n--- git status ---"
 git status
 
 
-echo
-echo -e "###################################"
-echo -e "### Update Repository: ~/xx-net ###"
-echo -e "###################################"
-echo -e "\n--- change directory ---"
-echo cd ~/xx-net
-cd ~/xx-net
-echo -e "\n--- git fetch --all ---"
-git fetch --all
-echo -e "\n--- git status ---"
-git status
+#echo
+#echo -e "###################################"
+#echo -e "### Update Repository: ~/xx-net ###"
+#echo -e "###################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/xx-net
+#cd ~/xx-net
+#echo -e "\n--- git fetch --all ---"
+#git fetch --all
+#echo -e "\n--- git status ---"
+#git status
 
 
 echo
@@ -215,17 +215,17 @@ echo -e "\n--- git status ---"
 git status
 
 
-echo
-echo -e "##################################"
-echo -e "### Update Repository: ~/cpython ###"
-echo -e "##################################"
-echo -e "\n--- change directory ---"
-echo cd ~/cpython
-cd ~/cpython
-echo -e "\n--- git fetch --all ---"
-git fetch --all
-echo -e "\n--- git status ---"
-git status
+#echo
+#echo -e "####################################"
+#echo -e "### Update Repository: ~/cpython ###"
+#echo -e "####################################"
+#echo -e "\n--- change directory ---"
+#echo cd ~/cpython
+#cd ~/cpython
+#echo -e "\n--- git fetch --all ---"
+#git fetch --all
+#echo -e "\n--- git status ---"
+#git status
 
 
 #print a new line at the end of the script
