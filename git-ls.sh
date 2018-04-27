1c9803e6a70e40779ff3dd0dd000901af8afd276
Chen Weixiang <weixiangx.chen@outlook.com>
2018-04-27 21:12:23 +0800

    Update script git-ls.sh

--- git-ls.sh
+++ git-ls.sh
@@ -1,44 +1,96 @@
 #!/bin/bash
 
+################################################################################
+# This Bash script is used to print the files changed by commit with absolute  #
+# path. The commit is specified by input parameter $1. Use HEAD as the commit  #
+# if no input parameter is specified.                                          #
+################################################################################
+
 #-------------------------------------------------------------------------------
-# This Bash script is used to print the files changed by commit with absolute
-# path. The commit is specified by input parameter $1. Use HEAD as the commit
-# if no input parameter $1 is specified.
-#
-# Usage: git-ls-commit-file.sh [commit-id]
+# 1) Global variables
 #-------------------------------------------------------------------------------
 
-#
-# 1) Get the commit and its parent
-#
+# Script name and location of this script
+scriptName="git-ls.sh"
 
-if [ "$1" == "" ]; then
-    cmt="HEAD"
-    parent_cmt="HEAD~"
-else
-    cmt=$1
-    parent_cmt=${cmt}~
-fi
+# Default commit ID
+cmt="HEAD"
+parent_cmt="HEAD~"
 
-#
-# 2) Get all changed files in <commit-id>~ and <commit-id>
-#
+# Show all commit files in one line by default if option '-n' is not specified
+newLine="No"
+
+#-------------------------------------------------------------------------------
+# 2) Usage
+#-------------------------------------------------------------------------------
+
+function usage(){
+    echo
+    echo "Usage:"
+    echo "${scriptName} [-h]"
+    echo "${scriptName} [-c <commit>] [-n]"
+    echo
+    echo "-c <commit>"
+    echo "    Specify the git commit that you want to check commit files of it."
+    echo "    If this option is not specified, use HEAD as commit ID by default."
+    echo
+    echo "-n"
+    echo "    By default, all commit files are shown in one line. One commit file"
+    echo "    per line if this option is specified."
+    echo
+    echo "-h"
+    echo "    Show the usage of this script."
+    echo
+}
+
+#-------------------------------------------------------------------------------
+# 3) Check script options
+#-------------------------------------------------------------------------------
+
+while getopts "c:nh" arg
+do
+    case ${arg} in
+        c)  # -c <commit>
+            cmt=${OPTARG}
+            parent_cmt=${OPTARG}~
+            ;;
+        n)  # -n
+            newLine="Yes"
+            ;;
+        h)  # -h
+            usage
+            exit 1
+            ;;
+        ?)  # unkonw argument
+            echo "ERROR: Unkonw argument"
+            exit 1
+            ;;
+    esac
+done
+
+#-------------------------------------------------------------------------------
+# 4) Get all changed files in <commit>~ and <commit>
+#-------------------------------------------------------------------------------
 
 files=`git diff-tree --no-commit-id --name-only -r ${parent_cmt} ${cmt}`
 
-#
-# 3) Print the absolute path of each file
-#
+#-------------------------------------------------------------------------------
+# 5) Print the absolute path of each file
+#-------------------------------------------------------------------------------
 
-toppath=`git rev-parse --show-toplevel`
+topPath=`git rev-parse --show-toplevel`
 
-unset absolute_files
+unset absoluteFiles
 
-for f in $files
-do
-    absolute_files+="$toppath/$f "
-done
-
-echo $absolute_files
+if [ ${newLine} == "Yes" ]; then
+    for f in $files; do
+        echo ${f}
+    done
+else
+    for f in $files; do
+        absoluteFiles+="${topPath}/${f} "
+    done
+    echo ${absoluteFiles}
+fi
 
 
