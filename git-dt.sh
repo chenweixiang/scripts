012158c19d1649974be09555436bb813bc7151d7
Weixiang Chen <weixiang.chen@outlook.com>
2019-10-10 11:55:04 +0800

    Update script git-dt.sh

--- git-dt.sh
+++ git-dt.sh
@@ -14,8 +14,11 @@
 scriptName="git-dt.sh"
 
 # Default commit ID
-commit="HEAD"
-parentCommit="HEAD~"
+currCommit="HEAD"
+prevCommit="HEAD~"
+
+# Default file
+file=
 
 #-------------------------------------------------------------------------------
 # 2) Usage
@@ -25,11 +28,20 @@ function usage(){
     echo
     echo "Usage:"
     echo "  ${scriptName} [-h]"
-    echo "  ${scriptName} [-c <commit>]"
+    echo "  ${scriptName} [-p <previous-commit>] [-c <current-commit>] [-f <file>]"
     echo
-    echo "  -c <commit>"
-    echo "     Specify the git commit that you want to check committed changes."
+    echo "  -p <previous-commit>"
+    echo "     Specify the previous commit that you want to check the changes from."
+    echo "     If this option is not specified, use HEAD~ as commit ID by default."
+    echo
+    echo "  -c <current-commit>"
+    echo "     Specify the current commit that you want to check the changes to."
     echo "     If this option is not specified, use HEAD as commit ID by default."
+    echo
+    echo "  -f <file>"
+    echo "     Specify the file with absolute- or relative path that you want to check"
+    echo "     changes between specified commits. If this option is not specified, show"
+    echo "     all changed files between specified commits by default."
     echo
     echo "  -h"
     echo "     Show the usage of this script."
@@ -40,12 +52,17 @@ function usage(){
 # 3) Check script options
 #-------------------------------------------------------------------------------
 
-while getopts "c:h" arg
+while getopts "c:p:f:h" arg
 do
     case ${arg} in
-        c)  # -c <commit>
-            commit=${OPTARG}
-            parentCommit=${OPTARG}~
+        p)  # -p <previous-commit>
+            prevCommit=${OPTARG}
+            ;;
+        c)  # -c <current-commit>
+            currCommit=${OPTARG}
+            ;;
+        f)  # -f <file>
+            file=${OPTARG}
             ;;
         h)  # -h
             usage
@@ -59,24 +76,36 @@ do
 done
 
 #-------------------------------------------------------------------------------
-# 4) Print log of specified commit
+# 4) Print log of specified commits
 #-------------------------------------------------------------------------------
 
 echo
-echo "###### Commit message of ${commit} ######"
+echo "###### 1) Short commit history from commit ${prevCommit} to ${currCommit} ######"
 echo
 
-git log --stat -1 -c --decorate --pretty=fuller ${commit}
+git log --date=short --pretty=format:'%Cgreen%h %Cred%ad %Cblue%cn %Cred%d %Creset%s' --graph --topo-order --decorate ${prevCommit}~..${currCommit}
+
+echo
+echo "###### 2) Detail commit history from commit ${prevCommit} to ${currCommit} ######"
+echo
+
+git log --stat -c --decorate --pretty=fuller ${prevCommit}~..${currCommit}
 
 #-------------------------------------------------------------------------------
-# 5) Use git difftool to show changes of each committed file
+# 5) Use git difftool to show changes
 #-------------------------------------------------------------------------------
 
-echo
-echo "###### Use 'git difftool' to show changes between commit ${parentCommit} and ${commit} ######"
-echo
-
-git difftool ${parentCommit} ${commit}
-
-echo
+if [ x${file} == x ]; then
+    echo
+    echo "###### 3) Use 'git difftool' to show all changes between commit ${prevCommit} and ${currCommit} ######"
+    echo
+    git difftool ${prevCommit} ${currCommit}
+    echo
+else
+    echo
+    echo "###### 3) Use 'git difftool' to show changes in file ${file} between commit ${prevCommit} and ${currCommit} ######"
+    echo
+    git difftool ${prevCommit} ${currCommit} -- ${file}
+    echo
+fi
 
