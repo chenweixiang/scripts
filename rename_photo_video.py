9e244a5f1577412842ff0ad29591f8cdb169478a
Weixiang Chen <weixiang.chen@outlook.com>
2020-12-13 20:12:31 +0800

    Update script rename_photo_video.py

--- rename_photo_video.py
+++ rename_photo_video.py
@@ -11,11 +11,37 @@
     ...
     IMG_20140315_091230_99.jpg
 
-本程序需要安装ExifRead模块，如果未改装
-改模块，可执行下列命令来安装：
-    sudo apt install python3-pip
-    pip3 --version
-    sudo pip3 install exifread
+本程序需要安装下列模块：
+    $ sudo apt install python3-pip
+    $ pip3 --version
+
+    $ sudo pip3 install exifread
+    $ pip3 show exifread
+      Name: ExifRead
+      Version: 2.3.2
+      Summary: Read Exif metadata from tiff and jpeg files.
+      Home-page: https://github.com/ianare/exif-py
+      Author: Ianaré Sévi
+      Author-email: ianare@gmail.com
+      License: BSD
+      Location: /usr/local/lib/python3.8/dist-packages
+      Requires: 
+      Required-by: 
+
+    $ pip3 install python-dateutil
+    $ pip3 show python-dateutil
+      Name: python-dateutil
+      Version: 2.8.1
+      Summary: Extensions to the standard Python datetime module
+      Home-page: https://dateutil.readthedocs.io
+      Author: Gustavo Niemeyer
+      Author-email: gustavo@niemeyer.net
+      License: Dual License
+      Location: /home/chenwx/.local/lib/python3.8/site-packages
+      Requires: six
+      Required-by: 
+
+    $ sudo apt install exiftool
 '''
 
 import os
@@ -23,7 +49,14 @@ import stat
 import time
 import exifread
 import argparse
+import subprocess
+from datetime import datetime
+from dateutil import tz
 from pathlib import Path
+
+# FILE_TYPE
+IMG_FILE_TYPE = "IMG_"
+VID_FILE_TYPE = "VID_"
 
 # Extension Names
 IMG_SUFFIX_FILTER = [ '.jpg', '.png', '.mpg', '.bmp', '.jpeg' ]
@@ -105,15 +138,15 @@ def isTargetFileType(filename):
     # 根据文件扩展名，判断是否是需要处理的文件类型
     filename_nopath = os.path.basename(filename)
     f, e = os.path.splitext(filename_nopath)
-    if e.lower() in IMG_SUFFIX_FILTER and handlePhoto == True:
-        return True, "IMG_"
-    elif e.lower() in VID_SUFFIX_FILTER and handleVedio == True:
-        return True, "VID_"
+    if e.lower() in IMG_SUFFIX_FILTER:
+        return True, IMG_FILE_TYPE
+    elif e.lower() in VID_SUFFIX_FILTER:
+        return True, VID_FILE_TYPE
     else:
         return False, ""
 
 
-def generateNewFileName(filename, prefix):
+def generateNewFileName(filename):
     # 根据照片的拍照时间生成新的文件名。如果获取不到拍照时间，则直接跳过
     try:
         if os.path.isfile(filename):
@@ -131,23 +164,45 @@ def generateNewFileName(filename, prefix):
     dirname = os.path.dirname(filename)
     filename_nopath = os.path.basename(filename)
     f, e = os.path.splitext(filename_nopath)
-
-    # 如果取得Exif信息，则根据照片的拍摄日期重命名文件
-    exifTags = exifread.process_file(fd)
-    if exifTags:
-        try:
-            # 取得照片的拍摄日期，并转换成 yyyymmdd_hhmmss 格式
-            t = exifTags [ 'EXIF DateTimeOriginal' ]
-            dateStr = prefix + str(t).replace(":", "")[:8] + "_" + str(t)[11:].replace(":", "")
-            # 生成新文件名
-            newFileName = os.path.join(dirname, dateStr + e).upper()
-            retVal = True            
-        except:
-            pass
+    
+    # Check if the file can be handled or not
+    isSupported, fileType = isTargetFileType(filename)
+    if isSupported == False:
+        return retVal, newFileName
+    
+    myDataFormat = fileType + '%Y%m%d_%H%M%S'
+    if fileType == IMG_FILE_TYPE and handlePhoto == True:
+        # 如果取得Exif信息，则根据照片的拍摄日期重命名文件
+        exifTags = exifread.process_file(fd)
+        if exifTags:
+            try:
+                # 取得照片的拍摄日期，并转换成 yyyymmdd_hhmmss 格式
+                t = exifTags [ 'EXIF DateTimeOriginal' ]
+                dateStr = fileType + str(t).replace(":", "")[:8] + "_" + str(t)[11:].replace(":", "")
+                # 生成新文件名
+                newFileName = os.path.join(dirname, dateStr + e).upper()
+                retVal = True            
+            except:
+                pass
+    elif fileType == VID_FILE_TYPE and handleVedio == True:
+        exiftoolCmd = "exiftool " + filename
+        exiftoolVal = subprocess.run(exiftoolCmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        metaData = str(exiftoolVal.stdout).split("\\n")
+        src_zone = tz.tzutc()
+        dst_zone = tz.tzlocal()
+        for element in metaData:
+            if "Media Create Date" in element:
+                mediaCreateData = (element.split(" : ")[1])
+                utcDateTime = datetime.strptime(mediaCreateData, '%Y:%m:%d %H:%M:%S')
+                utcDateTime = utcDateTime.replace(tzinfo=src_zone)
+                locDateTime = utcDateTime.astimezone(dst_zone)
+                dateStr = locDateTime.strftime(myDataFormat)
+                newFileName = os.path.join(dirname, dateStr + e).upper()
+                retVal = True
+                break
     
     # 如果获取Exif信息失败，则采用该照片的创建日期重命名文件
     if retVal == False and useModifiedDate == True:
-        myDataFormat = prefix + '%Y%m%d_%H%M%S'
         state = os.stat(filename)
         dateStr = time.strftime(myDataFormat, time.localtime(state[-2]))
         newFileName = os.path.join(dirname, dateStr + e).upper()
@@ -180,21 +235,19 @@ def scanDir(startdir):
     os.chdir(startdir)
     for obj in os.listdir(os.curdir):
         if os.path.isfile(obj):
-            retVal, prefix = isTargetFileType(obj)
+            # 对满足过滤条件的文件进行改名处理
+            retVal, newFileName = generateNewFileName(obj)
             if retVal:
-                # 对满足过滤条件的文件进行改名处理
-                retVal, newFileName = generateNewFileName(obj, prefix)
-                if retVal:
-                    try:
-                        if isExecute == True:
-                            os.rename(obj, newFileName)
-                        print(os.path.abspath(obj), " => ", newFileName)
-                    except:
-                        if isQuiet == False:
-                            print(os.path.abspath(obj), " => cannot rename to ", newFileName)
-                else:
+                try:
+                    if isExecute == True:
+                        os.rename(obj, newFileName)
+                    print(os.path.abspath(obj), " => ", newFileName)
+                except:
                     if isQuiet == False:
-                        print(os.path.abspath(obj), " =>  No change")
+                        print(os.path.abspath(obj), " => cannot rename to ", newFileName)
+            else:
+                if isQuiet == False:
+                    print(os.path.abspath(obj), " =>  No change")
         elif os.path.isdir(obj) and isRecursive == True:
             scanDir(obj)
             os.chdir(os.pardir)
